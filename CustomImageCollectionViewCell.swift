65e7d9e5f3c5b781edafb655e66b695034dc0832
amruthas0494 <90743732+amruthas0494@users.noreply.github.com>
2021-10-06 20:49:02 +0530

    Add files via upload

---
+++ CustomImageCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomImageCollectionViewCell.swift
+//  MySecondProject
+//
+//  Created by apple on 06/10/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+class CustomImageCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var viewImage: UIImageView!
+    
+}
