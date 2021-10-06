035ba33ce29509963866577cf1cdfc461c812813
amruthas0494 <90743732+amruthas0494@users.noreply.github.com>
2021-10-06 20:44:26 +0530

    Add files via upload

---
+++ CalculatorCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorCollectionViewCell.swift
+//  MySecondProject
+//
+//  Created by apple on 05/10/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    
+}
